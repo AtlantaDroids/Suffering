6425ec515b5bd68ef7f84cf3b5ef248a7a0dd309
at19809 <156552513+at19809@users.noreply.github.com>
2024-06-14 15:36:03 -0400

    Add Elevator Motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pain.java
@@ -8,7 +8,8 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @TeleOp
 public class Pain extends LinearOpMode {
 
-    private DcMotor[] motors = new DcMotor[4];
+    private DcMotor[] elevatorMotors = new DcMotor[2];
+    private DcMotor[] driveMotors = new DcMotor[4];
 
 
     private void applyConfig(DcMotor[] motors) {
@@ -20,25 +21,44 @@ public class Pain extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
+        /* Get Drive Motors */
+        driveMotors[0] = hardwareMap.get(DcMotor.class, "frontLeft");
+        driveMotors[1] = hardwareMap.get(DcMotor.class, "frontRight");
+        driveMotors[2] = hardwareMap.get(DcMotor.class, "backLeft");
+        driveMotors[3] = hardwareMap.get(DcMotor.class, "backRight");
 
-        motors[0] = hardwareMap.get(DcMotor.class, "frontLeft");
-        motors[1] = hardwareMap.get(DcMotor.class, "frontRight");
-        motors[2] = hardwareMap.get(DcMotor.class, "backLeft");
-        motors[3] = hardwareMap.get(DcMotor.class, "backRight");
+        /* Get Elevator Motors */
+        elevatorMotors[0] = hardwareMap.get(DcMotor.class, "elLeft");
+        elevatorMotors[1] = hardwareMap.get(DcMotor.class, "elRight");
 
-        motors[1].setDirection(DcMotorSimple.Direction.REVERSE);
-        motors[3].setDirection(DcMotorSimple.Direction.REVERSE);
+        /* Set Drive & Elevator Motor Direction */
+        driveMotors[1].setDirection(DcMotorSimple.Direction.REVERSE);
+        driveMotors[3].setDirection(DcMotorSimple.Direction.REVERSE);
+        elevatorMotors[1].setDirection(DcMotorSimple.Direction.REVERSE);
 
-        applyConfig(motors);
+        applyConfig(driveMotors);
+        applyConfig(elevatorMotors);
 
-
+        /* Run During Active OpMode */
         while(opModeIsActive()) {
+            double rt = -this.gamepad1.right_trigger;
+            double lt = -this.gamepad1.left_trigger;
             double y = -this.gamepad1.left_stick_y;
             double x = -this.gamepad1.right_stick_x;
 
-            if (this.gamepad1.a) {
-                // do the thing
+            for(DcMotor motor : elevatorMotors) {
+                motor.setPower(rt-lt);
             }
+
+            /* Set Drive Motors Power */
+            driveMotors[0].setPower(y+x);
+            driveMotors[1].setPower(y-x);
+            driveMotors[2].setPower(y+x);
+            driveMotors[3].setPower(y-x);
+
+
+
+
         }
 
     }
