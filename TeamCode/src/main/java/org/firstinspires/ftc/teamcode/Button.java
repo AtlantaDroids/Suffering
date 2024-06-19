5a38ed738f44a1cb458a5ad6b5004df775188012
OrigamiKing79 <152706314+OrigamiKing79@users.noreply.github.com>
2024-06-19 17:29:28 -0400

    Update Button.java
    added button pressing, releasing, and holding detection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Button.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Button.java
@@ -8,4 +8,33 @@ package org.firstinspires.ftc.teamcode;
 
 
 public class Button {
+    public boolean oldState;
+    public boolean curState;
+    public void read(boolean state) {
+        this.oldState = curState;
+        this.curState = state;
+
+
+    }
+
+    public boolean onPress() {
+        if(oldState == false && curState == true) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean onRelease() {
+        if(oldState == true && curState == false) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean onHold() {
+        if(oldState == true && curState == true) {
+            return true;
+        }
+        return false;
+    }
 }
