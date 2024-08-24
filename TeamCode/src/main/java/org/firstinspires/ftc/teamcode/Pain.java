010a7822bef7a98f6a6a2fe33930ac738d317068
OrigamiKing79 <152706314+OrigamiKing79@users.noreply.github.com>
2024-08-24 11:49:18 -0400

    We drive now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pain.java
@@ -4,11 +4,12 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class Pain extends LinearOpMode {
 
-    private DcMotor[] elevatorMotors = new DcMotor[2];
+    private DcMotor[] elevatorMotors = new DcMotor[1];
     private DcMotor[] driveMotors = new DcMotor[4];
 
 
@@ -28,33 +29,38 @@ public class Pain extends LinearOpMode {
         driveMotors[3] = hardwareMap.get(DcMotor.class, "backRight");
 
         /* Get Elevator Motors */
-        elevatorMotors[0] = hardwareMap.get(DcMotor.class, "elLeft");
-        elevatorMotors[1] = hardwareMap.get(DcMotor.class, "elRight");
+        elevatorMotors[0] = hardwareMap.get(DcMotor.class, "ellevator");
+
 
         /* Set Drive & Elevator Motor Direction */
-        driveMotors[1].setDirection(DcMotorSimple.Direction.REVERSE);
-        driveMotors[3].setDirection(DcMotorSimple.Direction.REVERSE);
-        elevatorMotors[1].setDirection(DcMotorSimple.Direction.REVERSE);
+        driveMotors[0].setDirection(DcMotorSimple.Direction.REVERSE);
+        driveMotors[2].setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+//
+
 
         applyConfig(driveMotors);
         applyConfig(elevatorMotors);
 
+        waitForStart();
         /* Run During Active OpMode */
         while(opModeIsActive()) {
             double rt = -this.gamepad1.right_trigger;
             double lt = -this.gamepad1.left_trigger;
             double y = -this.gamepad1.left_stick_y;
             double x = -this.gamepad1.right_stick_x;
+            double r = -this.gamepad1.left_stick_x;
 
             for(DcMotor motor : elevatorMotors) {
                 motor.setPower(rt-lt);
             }
 
             /* Set Drive Motors Power */
-            driveMotors[0].setPower(y+x);
-            driveMotors[1].setPower(y-x);
-            driveMotors[2].setPower(y+x);
-            driveMotors[3].setPower(y-x);
+            driveMotors[0].setPower(y-x-r);
+            driveMotors[1].setPower(y+x+r);
+            driveMotors[2].setPower(y+x-r);
+            driveMotors[3].setPower(y-x+r);
 
             int i = 0;
 
